ae6b8759e8a7c8a9e798aa84a4a9961d8dde5651
tasos <tasos.kontogiorgos@gmail.com>
2017-12-31 16:01:15 -0500

    4

---
+++ src/main/java/org/tasos/core/PathAccessor.java
@@ -0,0 +1,79 @@
+package org.tasos.core;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PathAccessor
+{
+
+    interface Acc
+    {
+        Object get( Object o );
+    }
+
+    private class MapAcc  implements Acc
+    {
+        private final Object m_key;
+
+        MapAcc( Object key )
+        {
+            m_key = key;
+        }
+
+
+        @Override
+        public Object get(Object o)
+        {
+            return ((GenMap) o ).getGen( m_key );
+        }
+    }
+
+    private class ListAcc implements Acc
+    {
+        private final int m_index;
+
+        ListAcc( int index )
+        {
+            m_index = index;
+        }
+
+        @Override
+        public Object get( Object o )
+        {
+            return ((List) o ).get( m_index );
+        }
+    }
+
+    private final List< Acc > m_elements = new ArrayList<>();
+
+    public PathAccessor( Object ... raw )
+    {
+        for( Object r : raw )
+        {
+            if (r instanceof Integer)
+            {
+                int index = ( Integer )r;
+                m_elements.add( new ListAcc( index ));
+                continue;
+            }
+
+            if (r instanceof String)
+            {
+                m_elements.add( new MapAcc( r.toString() ));
+                continue;
+            }
+            throw new RuntimeException("Illegal type");
+        }
+    }
+
+
+    public Object get( Object o )
+    {
+        for( Acc acc : m_elements )
+        {
+            o = acc.get( o );
+        }
+        return o;
+    }
+
+}
